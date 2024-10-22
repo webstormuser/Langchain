b72702622319bece437214bff498fd310cd57602
Ashwini Kakde <kakdeashwini12@gmail.com>
2024-10-22 17:07:51 +0530

    simple  streamlit app for genai

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
 load_dotenv()
 
-## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
